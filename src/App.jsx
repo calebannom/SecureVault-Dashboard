fcd320ed377e07a9624b5e2472c3fdc555702196
Caleb Charles Annom <calebannom7@gmail.com>
2026-06-21 12:23:40 +0000

    Initial project setup: Vite + React scaffold

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div style={{ color: 'white', background: '#0B0F14', minHeight: '100vh', padding: '24px', fontFamily: 'monospace' }}>
+      <h1>SecureVault is loading...</h1>
+    </div>
+  )
+}
+
+export default App
